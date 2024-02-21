086d09bf13157349a65306c7df48b2ca6bec7bcc
Prathamjyot Singh <124479037+PrathamjyotSingh@users.noreply.github.com>
2024-02-21 13:20:21 +0530

    Add files via upload

---
+++ python_6.py
@@ -0,0 +1,8 @@
+i=1
+while i<=10:
+    j=1
+    while j<=i:
+         print(j)
+         j=j+1
+    print("\n")
+    i=i+1     
---
+++ python_7.py
@@ -0,0 +1,2 @@
+print(list(range(5,100,5)))
+print(list(range(-10,-20,-2)))
